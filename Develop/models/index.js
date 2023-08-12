59c05604dc95a43860a6fd9e0316995f990df9b1
js131993 <john.sanders4444@gmail.com>
2023-08-12 13:18:16 -0400

    model folder
    Updating model folder.

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+//many to many is the relationship between the tables.  Many tags can have many products and a tag can have many products
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+});
 
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product);
 
 module.exports = {
   Product,
